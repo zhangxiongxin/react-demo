b5eecd43f49184ae077f48dc86848b241ff9ccc9
zhangxiongxin <986192524@qq.com>
2023-06-08 17:09:01 +0800

    1 file modified

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,17 @@ import './style.css';
 
 export default function App() {
   const [count, setCount] = useState(0);
-  function add() {
-    setCount(count + 1);
+  function add(e, number) {
+    setCount(count + number);
   }
   return (
     <div>
-      <button className="btn" onClick={add}>
+      <button
+        className="btn"
+        onClick={(e) => {
+          add(e, 2);
+        }}
+      >
         添加
       </button>
       <p>{count}</p>
